3fa9afabe376907da1789af2955d734116ae75ab
elysee <mboussaemmanuelito@gmail.com>
2025-09-14 14:25:07 +0100

    fix(url): navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,13 +1,13 @@
 export function Navigation() {
   const handleNavigate = (path: string) => {
-    window.location.href = path;
+    window.location.href = "/" + path;
   };
 
   return (
     <div>
       <h1>Navigation</h1>
-      <button onClick={() => handleNavigate("/faile")}>faile</button>
-      <button onClick={() => handleNavigate("/test")}>test</button>
+      <button onClick={() => handleNavigate("faile")}>faile</button>
+      <button onClick={() => handleNavigate("test")}>test</button>
     </div>
   );
 }
